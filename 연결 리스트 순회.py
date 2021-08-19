f7f46c4c3d7c24061b88faf0fdcf6d70f766db35
leecmin <cmlee0913@naver.com>
2021-08-19 17:29:16 +0900

    08.19

---
+++ 연결 리스트 순회.py
@@ -0,0 +1,35 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> class Node:
+    def __init__(self, item):
+        self.data = item
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.nodeCount = 0
+        self.head = None
+        self.tail = None
+
+    def getAt(self, pos):
+        if pos < 1 or pos > self.nodeCount:
+            return None
+        i = 1
+        curr = self.head
+        while i < pos:
+            curr = curr.next
+            i += 1
+        return curr
+
+    def traverse(self):
+        travel = []
+        curr = self.head
+        while curr != None:
+            travel.append(curr.data)
+            curr = curr.next
+        return travel
+
+
+# 이 solution 함수는 그대로 두어야 합니다.
+def solution(x):
+    return 0
