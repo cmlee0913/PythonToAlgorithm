c6f2fc855a40cec6f5b56e8f4ccea95d1d75b887
leecmin <cmlee0913@naver.com>
2021-08-27 03:01:40 +0900

    08.27

--- 재귀적 이진탐색.py
+++ 재귀적 이진탐색.py
@@ -1,17 +1,10 @@
-Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def solution(L, x, l, u):
-    if 
-l + 1 == u
-:
+def solution(L, x, l, u):
+    if l + 1 == u:
         return -1
     mid = (l + u) // 2
     if x == L[mid]:
         return mid
     elif x < L[mid]:
-        return 
-solution(L,x,l,mid)
-
+        return solution(L,x,l,mid)
     else:
-        return 
-solution(L,x,mid,u)
+        return solution(L,x,mid,u)
--- 정렬된 리스트에 원소 삽입.py
+++ 정렬된 리스트에 원소 삽입.py
@@ -1,6 +1,4 @@
-Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def solution(L, x):
+ def solution(L, x):
     answer = []
     
     for i in L:
